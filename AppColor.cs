426e80275743dd7c5f2d6aa65f1fc0e4ee372500
Buhii <buithanhhieubuhi@gmail.com>
2023-04-28 11:10:58 +0700

    Update5

---
+++ AppColor.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolSetColorByFillter.UIApp;
+
+namespace ToolSetColorByFillter
+{
+    internal class AppColor : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            CreateUIApp.CreateUI<ColorTab>(application);
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded; ;
+        }
+
+    }
+}
---
+++ UIApp/ColorTab.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolSetColorByFillter.UIApp.UIBase;
+
+namespace ToolSetColorByFillter.UIApp
+{
+    [Tab("Color Fillter")]
+    public class ColorTab
+    {
+        [Panel("Color Fillter")]
+        public class PanelColor
+        {
+            [Button("Color Fillter", "ToolSetColorByFillter.SetColorCommand", LinkImage = "color.png", LongDescription = "This is Test")]
+            public class ColorButton
+            {
+
+            }
+        }
+    }
+}
